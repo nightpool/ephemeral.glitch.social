0bbbc23cb28f792a9bc1bc6a04648ec80f688dae
beatrix-bitrot <beatrix.bitrot@gmail.com>
2017-08-27 17:59:27 +0000

    update headers in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,8 +121,11 @@ Rails.application.configure do
     'Server'                 => 'Mastodon',
     'X-Frame-Options'        => 'DENY',
     'X-Content-Type-Options' => 'nosniff',
-    'X-XSS-Protection'       => '0',
     'Permissions-Policy'     => 'interest-cohort=()',
+    'X-XSS-Protection'        => '1; mode=block',
+    'Content-Security-Policy' => "frame-ancestors 'none'; object-src 'none'; script-src 'self' https://ephemeral-static.glitch.social 'unsafe-inline'; base-uri 'none';" , 
+    'Referrer-Policy'         => 'no-referrer, strict-origin-when-cross-origin',
+    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains; preload'
   }
 
   config.x.otp_secret = ENV.fetch('OTP_SECRET')
