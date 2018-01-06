f4fe72bb905c540a7742a1d1647b579e3bad7f19
beatrix-bitrot <beatrix.bitrot@gmail.com>
2018-01-06 18:31:09 +0000

    tighten csp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,7 +123,7 @@ Rails.application.configure do
     'X-Content-Type-Options' => 'nosniff',
     'Permissions-Policy'     => 'interest-cohort=()',
     'X-XSS-Protection'        => '1; mode=block',
-    'Content-Security-Policy' => "frame-ancestors 'none'; object-src 'none'; script-src 'self' https://ephemeral-static.glitch.social 'unsafe-inline'; base-uri 'none';" , 
+    'Content-Security-Policy' => "frame-ancestors 'none'; object-src 'none'; script-src 'self' https://ephemeral-static.glitch.social ; base-uri 'none';" , 
     'Referrer-Policy'         => 'no-referrer, strict-origin-when-cross-origin',
     'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains; preload'
   }
