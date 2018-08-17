7fadc6d32b04e64c97a426d59fe54a10832d538a
beatrix-bitrot <beatrix.bitrot@gmail.com>
2018-08-17 23:25:55 +0000

    change after create commit to after create

--- app/models/status.rb
+++ app/models/status.rb
@@ -299,11 +299,11 @@ class Status < ApplicationRecord
 
   def schedule_removal
     moon = Lunartic.today
-    removal_delay = (moon.percent_full * 220 + 20)
+    removal_delay = 1#(moon.percent_full * 220 + 20)
     RemovalWorker.perform_in(removal_delay.minutes, id)
   end
 
-  after_create_commit :schedule_removal
+  after_create :schedule_removal
   after_create_commit :store_uri, if: :local?
   after_create_commit :update_statistics, if: :local?
 
