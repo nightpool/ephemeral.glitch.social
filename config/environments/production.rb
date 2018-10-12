81a6cfb30292acf433e1484113eec6b3d39832af
beatrix-bitrot <beatrix.bitrot@gmail.com>
2018-10-12 00:34:50 +0000

    tidy production env headers section

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,9 +123,8 @@ Rails.application.configure do
     'X-Content-Type-Options' => 'nosniff',
     'Permissions-Policy'     => 'interest-cohort=()',
     'X-XSS-Protection'        => '1; mode=block',
-    'Content-Security-Policy' => "frame-ancestors 'none'; object-src 'none'; script-src 'self' https://ephemeral-static.glitch.social ; base-uri 'none';" , 
     'Referrer-Policy'         => 'same-origin',
-    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains; preload'
+    'Strict-Transport-Security' => 'max-age=63072000; includeSubDomains'
   }
 
   config.x.otp_secret = ENV.fetch('OTP_SECRET')
