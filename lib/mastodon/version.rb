fdf7510a25857e86dbb85c9c8ad2a7592973a45b
nightpool <nightpool@cybre.space>
2020-05-25 17:21:18 +0200

    edit link

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY') { 'nightpool/ephemeral.glitch.social' }
     end
 
     def source_base_url
